dd64652b4f2bd27f494a28a3e38eda88c943e39f
MarcoAntonioHL <marco.dex9@gmail.com>
2022-09-27 15:09:18 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Signin } from './pages/login/Signin';
+import { Signup } from './pages/login/Signup';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <Signin/>
-    </>
-  )
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Signin />} />
+				<Route path="signup" element={<Signup />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
--- src/pages/login/Signin.jsx
+++ src/pages/login/Signin.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import Divider from '@mui/material/Divider';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export const Signin = () => {
 	return (
 		<>
 			<Grid
+				columns={20}
 				container
 				spacing={1}
 				direction="row"
@@ -16,18 +17,48 @@ export const Signin = () => {
 				alignContent="center"
 				wrap="wrap"
 			>
-				<Grid item xs={12}>
-					PokeRanking
-				</Grid>
-				<Divider orientation="vertical" flexItem />
-				<Grid item xs={12}>
-					signin
-				</Grid>
-				<Grid item xs={12}>
-					<TextField label="Outlined secondary" color="secondary" focused />
-				</Grid>
-				<Grid item xs={12}>
-					<TextField label="Outlined secondary" color="secondary" focused />
+				<Grid item xs={4}>
+					<Grid
+						container
+						spacing={1}
+						direction="row"
+						justifyContent="center"
+						alignItems="center"
+						alignContent="center"
+						wrap="wrap"
+						sx={{
+							marginTop: 20,
+						}}
+					>
+						<Grid item xs={12}>
+							<TextField
+								fullWidth
+								label="Email"
+								color="secondary"
+								size="small"
+								focused
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								fullWidth
+								label="Password"
+								color="secondary"
+								size="small"
+								focused
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<Button fullWidth variant="contained" color="secondary">
+								<Link to="/signup">ingresar</Link>
+							</Button>
+						</Grid>
+						<Grid item xs={12}>
+							<Button fullWidth variant="contained" color="secondary">
+								Registrarse
+							</Button>
+						</Grid>
+					</Grid>
 				</Grid>
 			</Grid>
 		</>
