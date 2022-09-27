5debb6b5fc74517fe65e571b4c9f7cbef868a37c
MarcoAntonioHL <marco.dex9@gmail.com>
2022-09-27 13:31:41 -0500

    nuevos cambios

--- src/pages/login/Signin.jsx
+++ src/pages/login/Signin.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
-import Divider from '@mui/material/Divider'
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 export const Signin = () => {
-  return (
-    <>
-        <Grid
-          container
-          spacing={1}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          alignContent="center"
-          wrap="wrap"
-        >
-            <Grid item xs={4}>
-                PokeRanking
-            </Grid>
-            <Divider orientation="vertical" flexItem />
-            <Grid item xs={4}>
-                signin
-            </Grid>
-        </Grid>
-    </>
-  )
-}
+	return (
+		<>
+			<Grid
+				container
+				spacing={1}
+				direction="row"
+				justifyContent="center"
+				alignItems="center"
+				alignContent="center"
+				wrap="wrap"
+			>
+				<Grid item xs={4}>
+					PokeRanking
+				</Grid>
+				<Divider orientation="vertical" flexItem />
+				<Grid item xs={4}>
+					signin
+				</Grid>
+			</Grid>
+			<Box
+				component="form"
+				sx={{
+					'& > :not(style)': { m: 1, width: '25ch' },
+				}}
+				noValidate
+				autoComplete="off"
+			>
+				<TextField label="Outlined secondary" color="secondary" focused />
+				<TextField
+					label="Filled success"
+					variant="filled"
+					color="success"
+					focused
+				/>
+				<TextField
+					label="Standard warning"
+					variant="standard"
+					color="warning"
+					focused
+				/>
+			</Box>
+		</>
+	);
+};
